c1f8949c8d4048d168e899753105aa111c13e8f7
Francisco A.C <90883587+FranAC9724@users.noreply.github.com>
2022-12-06 20:54:41 -0600

    Agregar pruebas
    Se incluyen carpetas locales de pruebas para el parallel testing

---
+++ Simulación - Parallel Testing/ParallelTesting - Marketplace/src/paralleltesting/ParallelTesting.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package paralleltesting;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+import org.openqa.selenium.firefox.FirefoxDriver.*;
+import org.openqa.selenium.chrome.ChromeDriver.*;
+
+/**
+ *
+ * @author joses
+ */
+
+public class ParallelTesting {
+
+    public static void main(String[] args) {
+        ChromeTest chrome = new ChromeTest();
+       MozzilaTest mozzila = new MozzilaTest();
+        mozzila.prueba1();
+        //chrome.prueba1();
+    }
+
+}
