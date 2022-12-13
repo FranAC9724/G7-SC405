9cc2819434f48d340bda4a3ee7d470b86fbb3edc
Francisco A.C <90883587+FranAC9724@users.noreply.github.com>
2022-12-12 19:40:07 -0600

    Base proyecto final
    Pruebas automatizadas

---
+++ Proyecto Final/JavaApplication1/src/ProyectoFinal/Main.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProyectoFinal;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Fran
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+       int menu;
+
+       do{
+           menu = Integer.parseInt(JOptionPane.showInputDialog("Escoja una opcion: "
+                        + "\n1 - Prueba Automatizada 1"
+                        + "\n2 - Prueba Automatizada 2"
+                        + "\n3 - Prueba Automatizada 3"
+                        + "\n4 - Prueba Automatizada 4"
+                        + "\n5- Salir"));
+       
+       }while(menu != 5);
+    
+    }
+}
---
+++ Proyecto Final/JavaApplication1/src/ProyectoFinal/Pruebas.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProyectoFinal;
+
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.*;
+import org.openqa.selenium.firefox.FirefoxDriver.*;
+
+/**
+ *
+ * @author Fran
+ */
+public class Pruebas {
+    
+    String driverPath = "URL DEL DRIVER";
+    String url = "https://www.mopt.go.cr/";
+
+    static WebDriver driver;
+
+    public Pruebas() {
+        
+    }
+   
+    public void Prueba1() {
+        //Prueba 2
+        System.out.println("Prueba 1");
+        
+    }
+
+    public void Prueba2() {
+        //Prueba 4
+        System.out.println("Prueba 2");
+    }
+
+    public void Prueba3() {
+        //Prueba 5
+        System.out.println("Prueba 3");
+    }
+
+    public void Prueba4() {
+        //Prueba 7
+        System.out.println("Prueba 4");
+    }
+
+}
